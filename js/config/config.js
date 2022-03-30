4eb918ee43a78e293709147a9b240c584526519b
craig <craigparra@gmail>
2022-03-30 17:25:47 +1000

    initial commit

--- js/config/config.js
+++ js/config/config.js
@@ -9,6 +9,29 @@ export default ConfigFactory.getConfig({
             "/" : "warn",
         }
     },
+    "https://alt-html.github.io/random-ui-minimal/" :{
+        logging : {
+            format : "text",
+            level : {
+                "/" : "error",
+            }
+        },
+        api : {
+            url : 'https://futph74eyi.execute-api.ap-southeast-2.amazonaws.com'
+        }
+    },
+    "http://test-random-ui-minimal+s3-website-ap-southeast-2+amazonaws+com/" :{
+        logging : {
+            format : "text",
+            level : {
+                "/" : "verbose",
+                "@alt-javascript/cdi" : "verbose",
+            }
+        },
+        api : {
+            url : 'https://e26arlcbve.execute-api.ap-southeast-2.amazonaws.com/'
+        }
+    },
     "http://127+0+0+1:8080/" : {
         logging : {
             format : "text",
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import config from './config/config.js';
 import contexts  from './config/contexts.js';
-import { ApplicationContext } from 'https://cdn.jsdelivr.net/npm/@alt-javascript/cdi/dist/alt-javascript-cdi-esm.js';
+import { ApplicationContext } from 'https://cdn.jsdelivr.net/npm/@alt-javascript/cdi@2.0.2/dist/alt-javascript-cdi-esm.js';
 
 let applicationContext = new ApplicationContext({contexts, config});
 await applicationContext.start();
